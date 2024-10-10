43601f1af7bfb3448cad277ed9bde70c137891f9
noah-wasd3r <noah@wasd3r.xyz>
2024-10-10 17:17:07 +0900

    feat: add deposit wrapper

--- contracts/interfaces/ILido.sol
+++ contracts/interfaces/ILido.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.19;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 interface ILido is IERC20 {
-    function submit(address user) external payable returns (uint256 stETHAmount);
+    function submit(address referral) external payable returns (uint256 stETHAmount);
 }
